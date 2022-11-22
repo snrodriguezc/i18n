bf9c8136e1e480273b99c72dd8b10baa9ffd5d9d
snrodriguezc <sn.rodriguez@uniandes.edu.co>
2022-11-22 09:05:18 -0500

    i18n

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {FormattedDate, FormattedNumber} from 'react-intl';
 
 const Job = (props) => {
   return (
@@ -6,9 +7,26 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        <FormattedNumber
+          value={props.offer.salary}
+        />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber
+          value={props.offer.views}
+        />
+      </td>
     </tr>
   );
 };
